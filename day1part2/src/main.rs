54007fca2a3351cd3c5957cdde66d160fa08550f
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2025-12-01 20:48:04 +0000

    solved puzzle in bad way

--- day1part2/src/main.rs
+++ day1part2/src/main.rs
@@ -10,34 +10,25 @@ impl Default for State {
 }
 
 pub fn process(mut state: State, line: &str) -> State {
-    let offset = match (&line[0..1], &line[1..]) {
+    let mut offset = match (&line[0..1], &line[1..]) {
         ("L", x) => -x.parse::<i32>().unwrap(),
         ("R", x) => x.parse::<i32>().unwrap(),
         _ => unreachable!()
     };
 
-    // if we start at 0 and turn left the program will count that as a click
-    // but this would mean be double count a click from last time
-    // so if we are at 0 and turning left reduce score by 1 to compensate
-    if state.angle == 0 && offset < 0 {
-        state.score -= 1;
+    while offset != 0 {
+        offset -= offset.signum();
+        state.angle += offset.signum();
+        if state.angle == 100 {
+            state.angle = 0;
+        }
+        if state.angle == -1 {
+            state.angle = 99;
+        }
+        if state.angle == 0 {
+            state.score += 1;
+        }
     }
-
-    state.angle += offset;
-    
-    while state.angle >= 100 {
-        state.angle -= 100;
-        state.score += 1;
-    }
-
-    while state.angle < 0 {
-        state.angle += 100;
-        state.score += 1;
-    }
-
-    // if state.score < 100 {
-    //     println!("{}: angle: {} score: {}", line, state.angle, state.score);
-    // }
 
     return state;
 }
